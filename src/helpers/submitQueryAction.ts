8c33fe9717f0253b072dab9576819fc434c7f521
Mitch Ryan <mitch@diesdas.digital>
2025-05-23 14:41:34 +0200

    Fix erroneous server action detection

--- src/helpers/submitQueryAction.ts
+++ src/helpers/submitQueryAction.ts
@@ -6,39 +6,38 @@ import { getRedisClient } from "../../lib/getRedisClient.mjs";
 import { REDIS_QUERY_KEY } from "../../lib/constants.mjs";
 import { crawlerQueue } from "../../lib/crawlerQueue.mjs";
 
-export const submitQueryAction = createSafeAction(
-  z.object({
-    query: z
-      .string()
-      .trim()
-      .nonempty()
-      /**
-       * \u00e4 -> ä
-       * \u00f6 -> ö
-       * \u00fc -> ü
-       * \u00df -> ß
-       */
-      .regex(/^[a-z0-9\u00e4\u00f6\u00fc\u00df\r\n-]+$/) // alphanumeric. newlines, ü, ä and ö only
-      .transform((val) => val.replace(/\r\n/g, "\n")), // normalize CRLF to LF)
-  }),
-  async ({ query }) => {
-    const redisClient = await getRedisClient();
-
-    await redisClient.set(REDIS_QUERY_KEY, query);
-
+const schema = z.object({
+  query: z
+    .string()
+    .trim()
+    .nonempty()
     /**
-     * Set a baseline so that we don't get emailed about all of the _existing_
-     * results.
+     * \u00e4 -> ä
+     * \u00f6 -> ö
+     * \u00fc -> ü
+     * \u00df -> ß
      */
-    crawlerQueue.add(
-      { sendNotifications: false },
-      {
-        attempts: 3,
-      },
-    );
+    .regex(/^[a-z0-9\u00e4\u00f6\u00fc\u00df\r\n-]+$/) // alphanumeric. newlines, ü, ä and ö only
+    .transform((val) => val.replace(/\r\n/g, "\n")), // normalize CRLF to LF)
+});
 
-    return {
-      ok: true,
-    };
-  },
-);
+export const submitQueryAction = createSafeAction(schema, async ({ query }) => {
+  const redisClient = await getRedisClient();
+
+  await redisClient.set(REDIS_QUERY_KEY, query);
+
+  /**
+   * Set a baseline so that we don't get emailed about all of the _existing_
+   * results.
+   */
+  crawlerQueue.add(
+    { sendNotifications: false },
+    {
+      attempts: 3,
+    },
+  );
+
+  return {
+    ok: true,
+  };
+});
