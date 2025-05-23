ea12328f988c819f44ad46398be42b0ea4ab7080
Mitch Ryan <mitch@diesdas.digital>
2025-05-23 15:04:02 +0200

    Force page to be dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { REDIS_QUERY_KEY } from "../../lib/constants.mjs";
 import { getRedisClient } from "../../lib/getRedisClient.mjs";
 import { Homepage } from "../components/Homepage";
 
+export const dynamic = "force-dynamic";
+
 export default async function Page() {
   const redisClient = await getRedisClient();
 
