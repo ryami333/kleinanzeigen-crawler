4a3a4fd4bed85f3d7bacd282bcf10920cca0d479
Mitch Ryan <mitch@diesdas.digital>
2025-05-23 17:06:31 +0200

    Avoid reading non-existed secrets during build

--- auth.ts
+++ auth.ts
@@ -4,19 +4,19 @@ import fs from "node:fs";
 import path from "node:path";
 import { env } from "./lib/env.mjs";
 
-const AUTH_SECRET = fs.readFileSync(
-  path.resolve(process.cwd(), env.AUTH_SECRET_FILE),
-  "utf8",
-);
-
 export const { auth, handlers, signIn, signOut } = NextAuth({
   providers: [
-    Keycloak({
-      issuer: "https://auth.mitch-ryan.com/realms/master",
-      // baseURL: "https://kc.mitch-ryan.com",
-      clientId: "mitchs-kleinanzeigen-crawler",
-      clientSecret: AUTH_SECRET,
-      // idpLogout: true,
-    }),
+    // Using callback-style so that secret is not evaluated during build-time
+    () =>
+      Keycloak({
+        issuer: "https://auth.mitch-ryan.com/realms/master",
+        // baseURL: "https://kc.mitch-ryan.com",
+        clientId: "mitchs-kleinanzeigen-crawler",
+        clientSecret: fs.readFileSync(
+          path.resolve(process.cwd(), env.AUTH_SECRET_FILE),
+          "utf8",
+        ),
+        // idpLogout: true,
+      }),
   ],
 });
