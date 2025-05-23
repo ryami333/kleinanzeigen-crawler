034bebba731c62c4095d676e5d0d8f59a08a4417
Mitch Ryan <mitch@diesdas.digital>
2025-05-23 16:12:20 +0200

    Add browse page

---
+++ src/app/browse/page.tsx
@@ -0,0 +1,26 @@
+import { Anchor, List, ListItem } from "@mantine/core";
+import { REDIS_QUERY_KEY } from "../../../lib/constants.mjs";
+import { getRedisClient } from "../../../lib/getRedisClient.mjs";
+
+export default async function Page() {
+  const redisClient = await getRedisClient();
+
+  const currentValue = (await redisClient.get(REDIS_QUERY_KEY)) ?? "";
+
+  return (
+    <List>
+      {currentValue
+        .split("\n")
+        .map((input) => input.trim())
+        .map((query, index) => (
+          <ListItem key={index}>
+            <Anchor
+              href={`https://www.kleinanzeigen.de/s-berlin/${query}/k0l3331`}
+            >
+              {query}
+            </Anchor>
+          </ListItem>
+        ))}
+    </List>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,9 @@ import {
 } from "@mantine/core";
 import { Notifications } from "@mantine/notifications";
 import { useDisclosure } from "@mantine/hooks";
-import { IconAdjustments } from "@tabler/icons-react";
+import { IconAdjustments, IconEye } from "@tabler/icons-react";
+
+export const dynamic = "force-dynamic";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const [opened, { toggle }] = useDisclosure();
@@ -58,6 +60,11 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
                 href="/"
                 label="Settings"
                 leftSection={<IconAdjustments size={16} stroke={1.5} />}
+              />
+              <NavLink
+                href="/browse"
+                label="Browse"
+                leftSection={<IconEye size={16} stroke={1.5} />}
               />
             </AppShell.Navbar>
             <AppShell.Main>
