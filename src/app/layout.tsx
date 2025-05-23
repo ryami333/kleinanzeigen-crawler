5d2d38c4d2fb5d4528bc687bc1a3b9dfe0bd55fc
Mitch Ryan <mitch@diesdas.digital>
2025-05-23 16:44:22 +0200

    Remove redundant styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,10 +25,6 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link
-          rel="stylesheet"
-          href="https://unpkg.com/@mantine/core@7.4.2/styles.css"
-        />
       </head>
 
       <body>
