34081bb01831972a53dd91d4f01b543742f28d07
Mitch Ryan <mitch@diesdas.digital>
2025-05-24 16:10:32 +0200

    Update layout to force signin when no session is found, and prevent flash of light color scheme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,22 +2,29 @@ import "@mantine/core/styles.css";
 import "@mantine/notifications/styles.css";
 
 import { MantineProvider } from "@mantine/core";
-import { auth } from "../../auth";
+import { auth, signIn } from "../../auth";
 import { App } from "../components/App";
 
 export const dynamic = "force-dynamic";
 
 const Layout = async ({ children }: { children: React.ReactNode }) => {
-  await auth();
+  const session = await auth();
+
+  if (!session) {
+    return signIn();
+  }
+
+  const colorScheme = "dark" as const;
+
   return (
-    <html>
+    <html data-mantine-color-scheme={colorScheme}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
 
       <body>
-        <MantineProvider forceColorScheme="dark">
+        <MantineProvider forceColorScheme={colorScheme}>
           <App>{children}</App>
         </MantineProvider>
       </body>
