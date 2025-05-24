e57614958e6cd4063064c79515bf586157637d77
Mitch Ryan <mitch@diesdas.digital>
2025-05-24 17:21:30 +0200

    Configure keycloak client with env variables + secrets

--- auth.ts
+++ auth.ts
@@ -1,22 +1,18 @@
 import NextAuth from "next-auth";
 import Keycloak from "next-auth/providers/keycloak";
 import fs from "node:fs";
-import path from "node:path";
 import { env } from "./lib/env.mjs";
 
-export const { auth, handlers, signIn, signOut } = NextAuth({
-  providers: [
-    // Using callback-style so that secret is not evaluated during build-time
-    () =>
+export const { auth, handlers, signIn, signOut } = NextAuth(
+  // Using callback-style so that secret is not evaluated during build-time
+  () => ({
+    secret: fs.readFileSync(env.AUTH_SECRET_FILE, "utf-8"),
+    providers: [
       Keycloak({
         issuer: "https://auth.mitch-ryan.com/realms/master",
-        // baseURL: "https://kc.mitch-ryan.com",
-        clientId: "mitchs-kleinanzeigen-crawler",
-        clientSecret: fs.readFileSync(
-          path.resolve(process.cwd(), env.AUTH_SECRET_FILE),
-          "utf8",
-        ),
-        // idpLogout: true,
+        clientId: env.KEYCLOAK_CLIENT_ID,
+        clientSecret: fs.readFileSync(env.KEYCLOAK_CLIENT_SECRET_FILE, "utf-8"),
       }),
-  ],
-});
+    ],
+  }),
+);
