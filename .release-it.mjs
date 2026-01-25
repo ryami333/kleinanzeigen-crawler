e975716e35ad5e96dde2390ef02b245cdbd7695c
Mitch Ryan <mitch@diesdas.digital>
2026-01-25 11:03:15 +0100

    Add release pipeline with release-it

---
+++ .release-it.mjs
@@ -0,0 +1,25 @@
+export default {
+  git: {
+    commitMessage: "release: v${version}",
+    tagName: "v${version}",
+    requireCleanWorkingDir: true,
+    requireUpstream: true,
+    requireBranch: "main",
+    tag: true,
+    commit: true,
+  },
+  github: {
+    release: true,
+  },
+  npm: {
+    publish: false,
+  },
+  hooks: {
+    "before:init": [
+      "yarn prettier . --check",
+      "yarn tsc",
+      "yarn eslint .",
+      "yarn vite build",
+    ],
+  },
+};
