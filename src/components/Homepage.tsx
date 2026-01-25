d973eab4d0c08a42cc0641c8621813d28b6380f3
Mitch Ryan <mitch@diesdas.digital>
2026-01-25 16:20:30 +0100

    Implement react-hook-form on Homepage

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,38 +1,56 @@
 "use client";
 
-import { useTransition } from "react";
 import { submitQueryAction } from "../helpers/submitQueryAction";
-import { Button, Textarea, Text, Stack } from "@mantine/core";
+import { Button, Textarea, Text, Stack, TextInput } from "@mantine/core";
 import { notifications } from "@mantine/notifications";
+import { useFieldArray, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import z from "zod";
+
+const formValidationSchema = z.object({
+  query: z
+    .object({
+      id: z.string(),
+      value: z.string().trim().nonempty({ error: "Row cannot be empty" }),
+    })
+    .array()
+    .transform((input) => input.map((item) => item.value).join("\n")),
+});
 
 export const Homepage = ({ currentValue }: { currentValue: string }) => {
-  const [isSubmitting, startTransition] = useTransition();
+  const { register, control, handleSubmit, formState } = useForm({
+    resolver: zodResolver(formValidationSchema),
+    defaultValues: {
+      query: currentValue
+        .split("\n")
+        .map((item, index) => ({ id: String(index), value: item })),
+    },
+  });
 
-  const onSubmit: React.FormEventHandler = (e) => {
-    e.preventDefault();
-    startTransition(async () => {
-      if (!isSubmitting) {
-        const query = e.currentTarget.querySelector("textarea")?.value ?? "";
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "query",
+  });
 
-        try {
-          await submitQueryAction({ data: { query } });
+  const onSubmit = handleSubmit(async (formValues) => {
+    try {
+      console.log(formValues.query);
+      await submitQueryAction({ data: { query: formValues.query } });
 
-          notifications.show({
-            title: "Success",
-            message: "Successfully updated.",
-            color: "green",
-          });
-        } catch (e) {
-          notifications.show({
-            title: "Error",
-            message: "This query could not be submitted",
-            color: "red",
-          });
-          console.error(e);
-        }
-      }
-    });
-  };
+      notifications.show({
+        title: "Success",
+        message: "Successfully updated.",
+        color: "green",
+      });
+    } catch (e) {
+      notifications.show({
+        title: "Error",
+        message: "This query could not be submitted",
+        color: "red",
+      });
+      console.error(e);
+    }
+  });
 
   return (
     <Stack>
@@ -47,13 +65,26 @@ export const Homepage = ({ currentValue }: { currentValue: string }) => {
           rowGap: "0.5em",
         }}
       >
-        <Textarea
-          name="query"
-          rows={10}
-          defaultValue={currentValue ?? undefined}
-          style={{ flex: "1 auto" }}
-        />
-        <Button type="submit" disabled={isSubmitting} fullWidth>
+        {fields.map((field, index) => (
+          <TextInput
+            key={field.id}
+            defaultValue={currentValue ?? undefined}
+            style={{ flex: "1 auto" }}
+            error={
+              formState.errors.query?.[index]?.message ||
+              formState.errors.query?.[index]?.id?.message ||
+              formState.errors.query?.[index]?.value?.message
+            }
+            {...register(`query.${index}.value`)}
+          />
+        ))}
+        <Button
+          type="button"
+          onClick={() => append({ id: window.crypto.randomUUID(), value: "" })}
+        >
+          Add
+        </Button>
+        <Button type="submit" disabled={formState.isSubmitting} fullWidth>
           Submit
         </Button>
       </form>
