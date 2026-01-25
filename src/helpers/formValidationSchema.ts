703adcf7f010e51e337475ae56078b16699637b2
Mitch Ryan <mitch@diesdas.digital>
2026-01-25 18:38:19 +0100

    Store stringified objects instead of plain strings

--- src/helpers/formValidationSchema.ts
+++ src/helpers/formValidationSchema.ts
@@ -1,32 +1,6 @@
 import z from "zod";
-import { kleinanzeigenUrlSchema } from "./kleinanzeigenUrlSchema";
-
-const NONEMPTY_ERROR = "Row cannot be empty";
+import { querySchema } from "./querySchema";
 
 export const formValidationSchema = z.object({
-  query: z
-    .object({
-      id: z.string(),
-      value: z.union([
-        z
-          .string()
-          .trim()
-          .nonempty({ error: NONEMPTY_ERROR })
-          .pipe(kleinanzeigenUrlSchema),
-        z
-          .string() /**
-           * \u00e4 -> ä
-           * \u00f6 -> ö
-           * \u00fc -> ü
-           * \u00df -> ß
-           */
-          .trim()
-          .nonempty({ error: NONEMPTY_ERROR })
-          .regex(/^[a-z0-9\u00e4\u00f6\u00fc\u00df\r\n-]+$/, {
-            error:
-              "May contain alphanumeric characters only (including ü, ä, ö and ß) ",
-          }),
-      ]),
-    })
-    .array(),
+  queries: querySchema.array(),
 });
---
+++ src/helpers/querySchema.ts
@@ -0,0 +1,28 @@
+import z from "zod";
+import { kleinanzeigenUrlSchema } from "./kleinanzeigenUrlSchema";
+
+const NONEMPTY_ERROR = "Row cannot be empty";
+
+export const querySchema = z.object({
+  id: z.string(),
+  value: z.union([
+    z
+      .string()
+      .trim()
+      .nonempty({ error: NONEMPTY_ERROR })
+      .pipe(kleinanzeigenUrlSchema),
+    z
+      .string() /**
+       * \u00e4 -> ä
+       * \u00f6 -> ö
+       * \u00fc -> ü
+       * \u00df -> ß
+       */
+      .trim()
+      .nonempty({ error: NONEMPTY_ERROR })
+      .regex(/^[a-z0-9\u00e4\u00f6\u00fc\u00df\r\n-]+$/, {
+        error:
+          "May contain alphanumeric characters only (including ü, ä, ö and ß) ",
+      }),
+  ]),
+});
