cfdc4ea05508f18ce2c065bc1b6a01a0729b1c92
Mitch Ryan <mitch@diesdas.digital>
2026-01-26 14:55:05 +0100

    Add missing import extension

--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,5 @@
 import { MongoClient } from "mongodb";
-import { env } from "./worker-env";
+import { env } from "./worker-env.ts";
 
 export const db = new MongoClient(env.MONGODB_CONNECTION_STRING).db(
   "kleinanzeigen-crawler",
