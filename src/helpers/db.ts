a362a4fe81ff9335072e9bf884e88e4eea7c830d
Mitch Ryan <mitch@diesdas.digital>
2026-01-26 14:21:17 +0100

    Use explicit table name

--- src/helpers/db.ts
+++ src/helpers/db.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 import { env } from "./frontend-env";
 
-export const db = new MongoClient(
-  env.MONGODB_CONNECTION_STRING,
-).db(/* Use DB name from connection string */);
+export const db = new MongoClient(env.MONGODB_CONNECTION_STRING).db(
+  "kleinanzeigen-crawler",
+);
