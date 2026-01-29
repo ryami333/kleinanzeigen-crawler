63fff8b8977f32eafde4d46618d28f51877d77d1
Mitch Ryan <mitch@diesdas.digital>
2026-01-29 14:56:02 +0100

    Enable auth on the mongodb service

--- src/helpers/db.ts
+++ src/helpers/db.ts
@@ -1,6 +1,7 @@
 import { MongoClient } from "mongodb";
 import { env } from "./frontend-env";
+import fs from "node:fs";
 
-export const db = new MongoClient(env.MONGODB_CONNECTION_STRING).db(
-  "kleinanzeigen-crawler",
-);
+export const db = new MongoClient(
+  fs.readFileSync(env.MONGODB_CONNECTION_STRING_FILE, "utf8").trim(),
+).db("kleinanzeigen-crawler");
